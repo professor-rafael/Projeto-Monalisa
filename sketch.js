12c6a093e2c6b5f90ab0a78bf6c0aa6801370ecc
Rafael Assis Santos <101332916+professor-rafael@users.noreply.github.com>
2025-02-06 08:46:21 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,25 @@
+function setup() {
+createCanvas(400, 400);
+}
 
+function draw() {
+background("green");
+  fill("blue");
+  circle (200,200,300);
+  fill("white");
+  circle (150,150,60);
+  circle (250,150,60);
+  line(150,270,250,270);
+  fill("grey");
+  triangle(200,180,170,220,220,220);
+  line(123,115,178,113);
+  line(225,116,279,106);
+  circle(150, 150, 10);
+  circle(250, 150, 10);
+  
+  if(mouseIsPressed) { 
+    console.log(mouseX, mouseY);
+   
+}
+  
+}
